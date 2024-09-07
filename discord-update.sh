456e36c66a31f68a6d7a0b4634e4fd040be95d13
Julio Olivencia <julioolivencia891@gmail.com>
2024-09-07 17:43:08 -0300

    update discord

---
+++ discord-update.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+
+PACKAGE="discord.deb"
+USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
+FILE_PATH="$USER_HOME"/Downloads/"$PACKAGE"
+DOWNLOAD_URL="https://discordapp.com/api/download?platform=linux&format=deb"
+
+# make_task(task_name, task)
+make_task() {
+  task_name=$1
+  task_command=${@:2}
+
+  echo "Step: $task_name"
+  "$task_command"
+  if [ $? -ne 0 ]
+    then
+      echo "ERROR: $task fail."
+      exit 1
+  fi
+}
+
+uninstall() {
+  # check if discord is already installed
+  dpkg -l | grep discord > /dev/null
+  if [ $? -eq 0 ]
+    then
+      echo "Removing Discord..."
+      dpkg --remove discord
+  fi
+}
+
+download() {
+  wget -O $FILE_PATH $DOWNLOAD_URL
+}
+
+install() {
+  dpkg --install $FILE_PATH
+}
+
+clean() {
+  rm $FILE_PATH
+}
+
+echo "Discord Updater"
+
+make_task "Uninstalling" uninstall
+make_task "Downloading" download 
+make_task "Installing" install 
+make_task "Cleaning" clean 
+
+echo "Done!"
+
